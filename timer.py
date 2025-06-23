1b11d6f3edaae18aaceb366170dceac41479ebad
Bethany Tijerina <tijerinabethany0306@gmail.com>
2025-06-22 23:49:22 -0500

    countdown

--- timer.py
+++ timer.py
@@ -25,10 +25,17 @@ class PomodoroTimer:
             while self.work_time:#this will run the 25 minites and the 5 minutes
                 pass
     def countdown(self, minutes):
-        time.sleep(1)
+        seconds = minutes * 60
+        while seconds > 0: #once the timer hits 0 the loop ends and the timer is done.
+            min_left = seconds //60 #get the minutes
+            sec_left = seconds % 60 #get the remaining seconds
+            #example: 145 seconds will be 2 minutes and 25 seconds
+            print(f"{min_left:02d}:{sec_left:02d}") # :02d means show 2 digits example: 5 will be 05.
+            time.sleep(1) # helps slow the timer. wait 1 second before it continues.
+            seconds -= 1 #decreases the counter.
+        print("Time's up!!!")
         #thid will handal conversions and do MM:SS format
         #converts minutes to seconds.
         # Uses a loop to subtract 1 second at a time
         #print time each second
         #end when countdown is over
-        pass
