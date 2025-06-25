4ab2410cb7cf2bf19936ce9c2ca4587287b90509
SpyteByte <mtz-ant-onio@outlook.com>
2025-06-24 23:05:30 -0500

    started task

---
+++ task.py
@@ -0,0 +1,12 @@
+from timer import PomodoroTimer
+
+class Task:
+    def Task(self,task_name,task_amount):# AM: defines the task name and when object is made amount can be set to zero
+        self.task_name=task_name
+        self.task_amount=self.task_amount
+    def new_task(self,name):# AM: creates a new task and keeps track of task amounts
+        self.task_name=name
+        self.task_amount+=1
+        
+
+
