f5aac128ba5bee85acb7c3dc99d48409442f4c26
SpyteByte <mtz-ant-onio@outlook.com>
2025-06-24 23:10:33 -0500

    started task

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from timer import PomodoroTimer
+from task import Task
 
 def main():
+    work=input('Hey what are we working on Today? ')#Am:not sure about this yet
+    a=Task(work)
     study_timer = PomodoroTimer()
     study_timer.start_session()
 if __name__ =="__main__": # only run the code below if this file is run direclty 
