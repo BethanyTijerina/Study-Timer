3df95f30beff7035ea48bce205887d56608554ee
Bethany Tijerina <tijerinabethany0306@gmail.com>
2025-06-25 22:56:13 -0500

    Fixed and Added task counter

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from timer import PomodoroTimer
 from task import Task
 
 def main():
-    work=input('Hey what are we working on Today? ')#Am:not sure about this yet
-    a=Task(work)
-    study_timer = PomodoroTimer()
+    work=input('Hey! What are we working on Today? ')#Am:not sure about this yet
+    task=Task(work)
+    print(f"Let's get started on: {task.task_name}")
+    study_timer = PomodoroTimer(task)
     study_timer.start_session()
+    print(f"\nYou've worked on {task.task_name}, {task.task_amount} time!")
 if __name__ =="__main__": # only run the code below if this file is run direclty 
     main()
