7cea56b19e22dd582c8f70eed8d1346d70d08776
SpyteByte <mtz-ant-onio@outlook.com>
2025-06-30 08:53:09 -0500

    added due dates

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from task import Task
 
 def run_text_version():
     work=input('Hey! What are we working on Today? ')#Am:not sure about this yet
-    task=Task(work)
-    print(f"Let's get started on: {task.task_name}")
+    due=input('When is it due? ')
+    task=Task(work,due)
+    print(f"Let's get started on: {task.task_name} Due Date: {task.due_date}") #AM: Added due date string variable mm/dd/yyyy
     study_timer = PomodoroTimer(task)
     study_timer.start_session()
     print(f"\nYou've worked on {task.task_name}, {task.task_amount} time!")
--- task.py
+++ task.py
@@ -1,9 +1,10 @@
 #from timer import PomodoroTimer
 
 class Task:
-    def __init__(self,task_name):# AM: defines the task name and when object is made amount can be set to zero
+    def __init__(self,task_name,due_date):# AM: defines the task name and when object is made amount can be set to zero
         self.task_name=task_name
-        self.task_amount= 1 #the default starting point
+        self.task_amount= 0 #the default starting point
+        self.due_date=due_date
     def new_task(self):# AM: creates a new task and keeps track of task amounts
         self.task_amount+=1
         
