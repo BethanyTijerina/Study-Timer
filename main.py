f2095ebc465ae4e98c9bc0709a94600f7dc4eaa1
Bethany Tijerina <tijerinabethany0306@gmail.com>
2025-06-29 22:16:30 -0500

    Added Sean timer and modified it to work with goals

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from timer import PomodoroTimer
 from task import Task
 
-def main():
+def run_text_version():
     work=input('Hey! What are we working on Today? ')#Am:not sure about this yet
     task=Task(work)
     print(f"Let's get started on: {task.task_name}")
     study_timer = PomodoroTimer(task)
     study_timer.start_session()
     print(f"\nYou've worked on {task.task_name}, {task.task_amount} time!")
+
+def run_twoD_version():
+    from twoD_timer import launch_timer
+    launch_timer()
+
+def main():
+    print("Welcome to your Study Timer!")
+    print("Please choose your version: ")
+    print("1. Text-based Pomodoro (Terminal)")
+    print("2. Popup Window Pomodoro\n")
+    option = input("Enter 1 or 2: ")
+    if option == "1":
+        run_text_version()
+    elif option == "2":
+        run_twoD_version()
+    else:
+        print("Invalid choice")
+    
 if __name__ =="__main__": # only run the code below if this file is run direclty 
     main()
